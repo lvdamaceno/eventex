c4ce32d0120da58d9cca63e3c5f11cdcb3a381a9
lvdamaceno <lvdamaceno@gmail.com>
2016-02-16 00:46:58 -0300

    speakers

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 def home(request):
     speakers = [
         {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
-        {'name': 'Allan Turing', 'photo': 'http://hbn.link/turing-pic'}
+        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'}
     ]
     return render(request, 'index.html', {'speakers': speakers})
+
+
+def speaker_detail(request, slug):
+    from django.http import HttpResponse
+    return HttpResponse()
