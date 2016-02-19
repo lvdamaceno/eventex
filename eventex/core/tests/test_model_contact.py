144c6399db4e4edd8d3b842fc44d07f69e0af83b
lvdamaceno <lvdamaceno@gmail.com>
2016-02-19 01:33:32 -0300

    contatos

---
+++ eventex/core/tests/test_model_contact.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from eventex.core.models import Speaker, Contact
+
+
+
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name='Vinicius Damaceno',
+            slug='vinicius-damaceno',
+            photo='http://www.viniciusdamaceno.com.br/static/img/faceofboe.png'
+        )
+
+    def test_email(self):
+        contact = Contact.objects.create(speaker=self.speaker,kind=Contact.EMAIL,value='lvdamaceno@gmail.com')
+
+        self.assertTrue(Contact.objects.exists())
+
+    def test_phone(self):
+        contact = Contact.objects.create(speaker=self.speaker,kind=Contact.PHONE,value='91-980869474')
+
+        self.assertTrue(Contact.objects.exists())
+
+    def test_choices(self):
+        """Contact kind should be limited to E or P"""
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_str(self):
+        contact = Contact(speaker=self.speaker,kind=Contact.EMAIL,value='lvdamaceno@gmail.com')
+
+        self.assertEqual('lvdamaceno@gmail.com', str(contact))
